b98fbed24729c75da4e51938725c7fe0535f6acd
Aresrilatha <aresrilatha24@gmail.com>
2024-11-09 19:12:52 +0530

    commit Hashmap

---
+++ src/com/collection/HashMapEx.java
@@ -0,0 +1,14 @@
+package com.collection;
+
+import java.util.HashMap;
+
+public class HashMapEx {
+	public static void main(String args[]) {
+		HashMap<Integer,String> m= new HashMap<Integer,String>();
+		m.put(1, "Ravi");
+		m.put(5, "Hari");
+		m.put(20, "Ram");
+		m.put(30, "Seetha");
+		System.out.println(m);
+	}
+}
