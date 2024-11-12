cd0424f0814f37e08dab00df2e256ad73955699a
Aresrilatha <aresrilatha24@gmail.com>
2024-11-12 19:38:18 +0530

    commit

--- src/com/collection/HashMapEx.java
+++ src/com/collection/HashMapEx.java
@@ -4,11 +4,24 @@ import java.util.HashMap;
 
 public class HashMapEx {
 	public static void main(String args[]) {
-		HashMap<Integer,String> m= new HashMap<Integer,String>();
+		HashMap<Integer, String> m = new HashMap<Integer, String>();
 		m.put(1, "Ravi");
 		m.put(5, "Hari");
 		m.put(20, "Ram");
 		m.put(30, "Seetha");
-		System.out.println(m);
+		m.put(15, "Krishna");
+		System.out.println(m+"\n");
+		
+		System.out.println(m.get(5));
+		System.out.println(m.containsKey(20));
+		System.out.println(m.containsValue("Ram"));
+		System.out.println(m.remove(1));
+		System.out.println(m.replace(15, "Hanuman"));
+		System.out.println(m.size());
+		System.out.println(m.keySet()); // keys are arranged in the form of set
+		System.out.println(m.values());
+		
+		System.out.println("\n"+m);
+		
 	}
 }
