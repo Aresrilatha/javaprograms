d343ab5f6b24cb38b0a4a4db43ad4609e0485ee9
Aresrilatha <aresrilatha24@gmail.com>
2024-11-12 19:39:03 +0530

    commit

--- src/com/collection/LinkedListEx.java
+++ src/com/collection/LinkedListEx.java
@@ -17,10 +17,23 @@ public class LinkedListEx {
 		lst1.add(11);
 		lst1.add(12);
 		lst1.add(13);
-		System.out.println(lst1);
+		lst1.add(5);
+		lst1.add(8);
+		System.out.println(lst1+"\n");
+		
+		System.out.println(lst1.contains(4));
+		System.out.println(lst1.lastIndexOf(5));
+		System.out.println(lst1.offer(18)); // adds the element at the end 
+		System.out.println(lst1.indexOf(7));
+		System.out.println(lst1.remove(0)); // removed element at particular index
+		System.out.println(lst1.removeFirstOccurrence(5));
+		System.out.println(lst1.get(6));
+		System.out.println(lst1.size());
+		System.out.println(lst1.set(3, 15));
+		System.out.println(lst1.element());// returns the  head element
 		
 		//OR
-		
+		System.out.println("\n\n");
 		for(int i=0;i<lst1.size();i++) {
 			System.out.println(lst1.get(i));
 		}
